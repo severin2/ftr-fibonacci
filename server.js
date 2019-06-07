0a582267323f49d2c0b2023e8a2dba3a639368c0
Severin Bartmess <severin.bartmess@gmail.com>
2019-06-06 18:59:19 -0600

    Removed refs to .maps in prod

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/health', function (req, res) {
- return res.sendStatus(200);
-});
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.get('/health', function (req, res) {
+  return res.sendStatus(200);
+ });
+
 app.listen(port, () => {
   process.stdout.write(`Static app listening at ${port}\n`);
 });
